43b4580c09ac09d065bc02a69c2309ad8eaac310
Zach Smelcer <zacaronithesmelcer@icloud.com>
2021-04-03 21:21:52 -0500

    Updated the search feature to search the user's desired search instead of a query

--- client/src/Components/Pages/SearchPage/SearchForm.js
+++ client/src/Components/Pages/SearchPage/SearchForm.js
@@ -1,29 +1,43 @@
 import React from "react";
-import {Button} from "reactstrap";
+import { Button } from "reactstrap";
 
-function SearchForm(props){
-    return(
-        <form style={{backgroundColor: 'black', marginBottom: 30}}>
-        <div className="form-group" style={{border: '1px solid black', marginLeft: 10, marginRight: 10, padding: 10}}>
-          <h3 style={{textAlign: 'left', color: 'whitesmoke'}}>Book Search</h3>
-          <br/>
-          
-          <label htmlFor="search" style={{color: 'whitesmoke'}}>Book:</label>
-          <input
-            onChange={props.handleInputChange}
-            value={props.search}
-            name="search"
-            type="text"
-            className="form-control"
-            placeholder="Search Google Books"
-            id="search"
-          />
-          <Button color="success" style={{marginTop: 10}} onClick={props.handleFormSubmit}>
-            SEARCH
-          </Button>
-        </div>
-      </form>
-    );
-};
+function SearchForm(props) {
+  return (
+    <form style={{ backgroundColor: "black", marginBottom: 30 }}>
+      <div
+        className="form-group"
+        style={{
+          border: "1px solid black",
+          marginLeft: 10,
+          marginRight: 10,
+          padding: 10,
+        }}
+      >
+        <h3 style={{ textAlign: "left", color: "whitesmoke" }}>Book Search</h3>
+        <br />
+
+        <label htmlFor="search" style={{ color: "whitesmoke" }}>
+          Book:
+        </label>
+        <input
+          onChange={props.handleInputChange(text)}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search Google Books"
+          id="search"
+        />
+        <Button
+          color="success"
+          style={{ marginTop: 10 }}
+          onClick={props.handleFormSubmit}
+        >
+          SEARCH
+        </Button>
+      </div>
+    </form>
+  );
+}
 
 export default SearchForm;
--- client/src/Components/Pages/SearchPage/SearchPage.js
+++ client/src/Components/Pages/SearchPage/SearchPage.js
@@ -1,61 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import BookBlock from "./BookBlock.js";
 import API from "../../../utils/API";
 import SearchForm from "./SearchForm";
 
-
 class SearchPage extends Component {
-
   state = {
     results: [],
     result: {},
-    search: ""
+    search: "",
   };
 
-  searchBooks = query => {
-
+  searchBooks = (query) => {
+    let query = this.state.search;
     API.search(query)
-      .then(res => this.setState({ results: res.data.items }))
-      .catch(err => {
+      .then((res) => this.setState({ results: res.data.items }))
+      .catch((err) => {
         if (err) console.log(err);
         window.location.reload();
         alert("Could not find your search, please try a different book");
       });
   };
-  
-  
-  handleInputChange = event => {
-    const name = event.target.name;
-    const value = event.target.value;
+
+  handleInputChange = (text) => {
     this.setState({
-      [name]: value
+      search: text,
     });
   };
 
-
-
- 
-  handleFormSubmit = event => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
     this.searchBooks(this.state.search);
   };
 
-  render(){
-    return(
-        <div>
+  render() {
+    return (
+      <div>
         <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-              />
-    <div style={{borderBlock: '1px solid black',width: '100%', padding: 10, backgroundColor: '#707070'}}>
-        <h5 style={{color: 'whitesmoke'}}>Results:</h5>
-        <BookBlock  books={this.state.results}/>
-    </div>
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        <div
+          style={{
+            borderBlock: "1px solid black",
+            width: "100%",
+            padding: 10,
+            backgroundColor: "#707070",
+          }}
+        >
+          <h5 style={{ color: "whitesmoke" }}>Results:</h5>
+          <BookBlock books={this.state.results} />
+        </div>
       </div>
-
     );
-  };
-};
+  }
+}
 
 export default SearchPage;
